92a6daaa5b8d06ec873701ada9384b59354903c8
qsef <66955882+11xdev-coder@users.noreply.github.com>
2024-08-31 13:03:37 +0200

    Moved FNA to solution

--- BlasterMaster/obj/Debug/net8.0/BlasterMaster.AssemblyInfo.cs
+++ BlasterMaster/obj/Debug/net8.0/BlasterMaster.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlasterMaster")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+897822840039c6bf3ab04cb1b7cea6b62e3c5f84")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlasterMaster")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlasterMaster")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
