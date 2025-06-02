19bfbae1d3e234290fade027a35a628b930dfc84
Matvei Piashchynski <66955882+11xdev-coder@users.noreply.github.com>
2025-06-03 01:30:04 +0300

    inventory slot image change

--- Blastia/obj/Debug/net8.0/Blastia.AssemblyInfo.cs
+++ Blastia/obj/Debug/net8.0/Blastia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blastia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+835a5b4a30cb43cc7c1ea66b1c9a03328839fb20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1eb2ec314eef5e5d6803d4393c4117322c9b29bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blastia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blastia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
